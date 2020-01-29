303aedea478c53967f51f574761797d2bc697a27
unknown <avdhesh7394vn@gmail.com>
2020-01-29 23:53:37 +0530

    island generated procedurely

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public int dimension;
+
+    public int[,] islandGrid;
+
+    public GameObject basicUnit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        islandGrid = new int[dimension, dimension];
+
+        CreateIsland();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CreateIsland() {
+        for (int i = 0; i < dimension; i++) {
+            for (int j = 0; j < dimension; j++) {
+                islandGrid[i, j] = Random.Range(0, 2);
+            }
+        }
+
+        int z = -dimension / 2;
+
+        for (int i = 0; i < dimension; i++) {
+            int x = -dimension / 2;
+            for (int j = 0; j < dimension; j++) {
+                if (islandGrid[i, j] == 1) {
+                    float heightRange = Random.Range(0.3f, 0.7f);
+                    Instantiate(basicUnit, new Vector3(x, heightRange, z), Quaternion.identity);
+                }
+
+                x++;
+            }
+
+            z++;
+        }
+    }
+}
