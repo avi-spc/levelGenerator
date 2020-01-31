3c1d628dac2ff4bb232de7681a87302d50f97e2b
unknown <avdhesh7394vn@gmail.com>
2020-01-31 10:53:35 +0530

    data written to json successfully

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -1,50 +1,96 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using LitJson;
+using System.IO;
+
+public struct Islands {
+    public Vector3 position { get; set; }
+    public string islandConfiguration { get; set; }   
+}
+
+public class LevelInfo
+{
+    public int numOfIslands { get; set; }
+    public int dimensions { get; set; }
+    public List<Islands> islands;
+
+}
 
 public class LevelGenerator : MonoBehaviour
 {
-    public int dimension;
+    static int dimension;
 
     public int[,] islandGrid;
 
     public GameObject basicUnit;
 
-    // Start is called before the first frame update
+    string islandBinary = "";
+    List<Islands> islands = new List<Islands>();
+    LevelInfo levelInfo = new LevelInfo();
+    JsonData islandJson;
     void Start()
     {
+        dimension = 5;
         islandGrid = new int[dimension, dimension];
-
-        CreateIsland();   
+        CreateIsland();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void CreateIsland() {
-        for (int i = 0; i < dimension; i++) {
-            for (int j = 0; j < dimension; j++) {
-                islandGrid[i, j] = Random.Range(0, 2);
+        for (int k = 0; k < 2; k++) {
+            islandBinary = "";
+            for (int i = 0; i < dimension; i++)
+            {
+                for (int j = 0; j < dimension; j++)
+                {
+                    islandGrid[i, j] = Random.Range(0, 2);
+
+                    if (i == dimension - 1 && j == dimension - 1)
+                        islandBinary += islandGrid[i, j];
+                    else
+                        islandBinary += islandGrid[i, j] + " ";
+                }
             }
-        }
 
-        int z = -dimension / 2;
+            int z = -dimension / 2;
 
-        for (int i = 0; i < dimension; i++) {
-            int x = -dimension / 2;
-            for (int j = 0; j < dimension; j++) {
-                if (islandGrid[i, j] == 1) {
-                    float heightRange = Random.Range(0.3f, 0.7f);
-                    Instantiate(basicUnit, new Vector3(x, heightRange, z), Quaternion.identity);
+            for (int i = 0; i < dimension; i++)
+            {
+                int x = -dimension / 2;
+                for (int j = 0; j < dimension; j++)
+                {
+                    if (islandGrid[i, j] == 1)
+                    {
+                        float heightRange = Random.Range(0.3f, 0.7f);
+                        Instantiate(basicUnit, new Vector3(x, heightRange, z), Quaternion.identity);
+                    }
+
+                    x++;
                 }
 
-                x++;
+                z++;
             }
 
-            z++;
+            Islands newIsland = new Islands();
+            newIsland.position = Vector3.zero;
+            newIsland.islandConfiguration = islandBinary;
+
+            islands.Add(newIsland);
+
         }
+
+        levelInfo.numOfIslands = 2;
+        levelInfo.dimensions = dimension;
+        levelInfo.islands = islands;
+        islandJson = JsonMapper.ToJson(levelInfo);
+        File.WriteAllText(Application.dataPath + "/level.json", islandJson.ToString());
     }
 }
+
+
